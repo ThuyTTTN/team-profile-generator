5b38a1ee074f2156275417c90d08863ed7451f7a
Thuy Nguyen <ttruong06@gmail.com>
2022-04-26 10:28:37 -0400

    test engineer and intern js

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,9 +1,9 @@
 const Engineer = require ('../lib/Engineer');
 
 test ('create an engineer gitHub and return of Manager', () => {
-    const manager = new Manager('Bob', 1, 'test@test.com', 123);
+    const engineer = new Engineer('Sue', 2, 'test2@test.com', 'banana');
 
     expect(engineer.github).toBe('banana');
-    expect(engineer.github()).toBe('banana');
+    expect(engineer.getGithub()).toBe('banana');  
     expect(engineer.getRole()).toBe('Engineer');
 })
